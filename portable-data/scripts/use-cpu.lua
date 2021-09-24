612912e73cbada427b54fdb2925d807d77d499f7
banana <37802190+422658476@users.noreply.github.com>
2021-09-24 16:04:40 +0800

    Switch according to the encoding format of the video
    Now the script can automatically switch whether to use cpu or gpu according to the video encoding format and video width

--- portable-data/scripts/use-cpu.lua
+++ portable-data/scripts/use-cpu.lua
@@ -5,18 +5,46 @@
 --local msg = require("mp.msg")
 
 --如果视频的宽度大于3000或者视频的高度大于2000，那么自动切换回使用cpu解码
+function use_cpu_or_gpu(w_num,h_num)
+			local hwdec_value = mp.get_property("hwdec")
+			local hwdec_format = mp.get_property("video-format")
+			local which_bigger = ""
+			if w_num > 0 then
+				which_bigger = "width=" .. w_num
+			else
+				which_bigger = "heigth=" .. h_num
+			end
+			
+			if hwdec_format == "hevc" then
+			--视频使用的是h.265视频格式
+					--h.265如果导致cpu 100% gpu不会100%，那么自动切换到使用gpu
+					if hwdec_value == "no" then
+					--正在使用cpu
+						mp.set_property("hwdec", "auto-safe")
+						mp.command("show-text \"" .. which_bigger .. " video:${video-format} switch to GPU\"")
+					else
+						mp.command("show-text \"" .. which_bigger .. " video:${video-format} using GPU\"")
+					end
+			else
+			--视频使用的是非h.265视频格式,比如h.264
+					--非h.265视频格式,比如h.264，如果导致cpu不会100% gpu 100%，那么自动切换到使用cpu解码
+					if hwdec_value == "no" then
+					--正在使用cpu
+						mp.command("show-text \"" .. which_bigger .. " video:${video-format} using CPU\"")
+					else
+						mp.set_property("hwdec", "no")
+						mp.command("show-text \"" .. which_bigger .. " video:${video-format} switch to CPU\"")
+					end
+			end
+end
 
 function w_change(name, value)
 	--注意width的值可能等于nil，如果没有做这个判断，那么lua会报错和脚本无法使用， 可以使用require("mp.msg")查看错误
 	if value ~= nil then
 		if value >= 3000 then
-			local hwdec_value = mp.get_property("hwdec")
-			if hwdec_value == "no" then
-			--正在使用cpu，无需任何操作
-			else
-				mp.set_property("hwdec", "no")
-				mp.command("show-text \"width>3000 using cpu\"")
-			end
+
+			 use_cpu_or_gpu(value,0)
+
 		end
 	end
 
@@ -26,13 +54,9 @@ function h_change(name, value)
 	--注意width的值可能等于nil，如果没有做这个判断，那么lua会报错和脚本无法使用， 可以使用require("mp.msg")查看错误
 	if value ~= nil then
 		if value >= 2000 then
-			local hwdec_value = mp.get_property("hwdec")
-			if hwdec_value == "no" then
-			--正在使用cpu，无需任何操作
-			else
-				mp.set_property("hwdec", "no")
-				mp.command("show-text \"height>2000 using cpu\"")
-			end
+		
+			 use_cpu_or_gpu(0,value)
+			
 		end
 	end
 	
@@ -40,4 +64,4 @@ end
 
 
 mp.observe_property( "width", "number", w_change)
-mp.observe_property( "height", "number", h_change)
+--mp.observe_property( "height", "number", h_change)
