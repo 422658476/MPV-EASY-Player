d6370d761a57e2d5bd77db02aac12eb9b625782e
banana <37802190+422658476@users.noreply.github.com>
2023-11-25 15:31:24 +0800

    Add files via upload

--- portable-data/scripts/osd-bar.lua
+++ portable-data/scripts/osd-bar.lua
@@ -6,21 +6,47 @@ local osd_duration_value_second = osd_duration_value / 1000
 local osd_bar_value = mp.get_property_osd("osd-bar")
 local osd_level_value = mp.get_property_osd("osd-level")
 
+
+
 function osdbar_show_hide_with_fullscreen(name, param)
 	if param == true then
+			--使用stop()大部分时间会比kill()更快
 			osd_bar_show_forever:stop()
 	else
-	--这一行是为了运行后就立刻显示osd-bar，而不是等待osd_duration_value_second秒后才开始一直显示
-			mp.command("osd-bar show-progress")
-		osd_bar_show_forever = mp.add_periodic_timer( osd_duration_value_second, function()
+			--这一行是为了退出全屏后立刻显示osd-bar，而不是等待osd_duration_value_second秒后才开始一直显示
 			mp.command("osd-bar show-progress")
 			osd_bar_show_forever:resume()
-		end)
 	end
 end
 
+function file_loaded(event)
+
+	local fullscreen_value = mp.get_property("fullscreen")
+
+	if fullscreen_value == "yes" then
+		--如果启动时直接进入全屏，那么不执行快速显示osd bar的命令
+	else
+		mp.command("osd-bar show-progress")
+	end
+
+end
+
+
+
+
 if osd_bar_value ~= "no" and osd_level_value >= "1" then
---下面一行是当mpv进入全屏状态时加载function，退出全屏状态时加载function
-	mp.observe_property("fullscreen", "bool", osdbar_show_hide_with_fullscreen)
+
+		--这一行是为了运行后就立刻显示osd-bar，而不是等待osd_duration_value_second秒后才开始一直显示
+		--加载文件时
+		mp.register_event("file-loaded", file_loaded)
+
+		--mp.add_periodic_timer需要写在最前面，在没有加载这个前调用osd_bar_show_forever:kill()等命令会让mpv崩溃
+		osd_bar_show_forever = mp.add_periodic_timer( osd_duration_value_second, function()
+					mp.command("osd-bar show-progress")
+				--osd_bar_show_forever:resume()
+		end)
+
+		--下面一行是当mpv进入全屏状态时加载function，退出全屏状态时加载function
+		mp.observe_property("fullscreen", "bool", osdbar_show_hide_with_fullscreen)
 
 end
