ea6c868045e24a2b128f6a866d73f9dfa6c18171
banana <37802190+422658476@users.noreply.github.com>
2024-11-30 21:10:57 +0800

    Add files via upload

--- portable-data/scripts/default-input.lua
+++ portable-data/scripts/default-input.lua
@@ -74,37 +74,37 @@ end
 
 --鼠标和键盘音量+
 mp.add_key_binding("WHEEL_UP", "default_volume_up_a",  function() mp.command("add volume 2;"..settings.default_volume_value) end)
-mp.add_key_binding("0", "default_volume_up_b",  function() mp.command("add volume 2;"..settings.default_volume_value) end)
+mp.add_key_binding("0", "default_volume_up_b",  function() mp.command("add volume 2;"..settings.default_volume_value) end, "repeatable")
 
 --鼠标和键盘音量-
 mp.add_key_binding("WHEEL_DOWN", "default_volume_down_a",  function() mp.command("add volume -2;"..settings.default_volume_value) end)
-mp.add_key_binding("9", "default_volume_down_b",  function() mp.command("add volume -2;"..settings.default_volume_value) end)
+mp.add_key_binding("9", "default_volume_down_b",  function() mp.command("add volume -2;"..settings.default_volume_value) end, "repeatable")
 
 --字幕和音轨切换
 mp.add_key_binding("ctrl+f", "default_cycle_sub",  function() mp.command("cycle sub;"..settings.default_cycle_sub_value) end)
 mp.add_key_binding("ctrl+g", "default_cycle_audio",  function() mp.command("cycle audio;"..settings.default_cycle_audio_value) end)
 
 --方向键跳转进度
-mp.add_key_binding("RIGHT", "default_seek_up",  function() mp.command("seek  10") end)
-mp.add_key_binding("LEFT", "default_seek_down",  function() mp.command("seek  -10") end)
-mp.add_key_binding("UP", "default_seek_up_up",  function() mp.command("seek  60") end)
-mp.add_key_binding("DOWN", "default_seek_down_down",  function() mp.command("seek  -60") end)
+mp.add_key_binding("RIGHT", "default_seek_up",  function() mp.command("seek  10") end, "repeatable")
+mp.add_key_binding("LEFT", "default_seek_down",  function() mp.command("seek  -10") end, "repeatable")
+mp.add_key_binding("UP", "default_seek_up_up",  function() mp.command("seek  60") end, "repeatable")
+mp.add_key_binding("DOWN", "default_seek_down_down",  function() mp.command("seek  -60") end, "repeatable")
 
 --对比度
-mp.add_key_binding("2", "default_contrast_up",  function() mp.command("add contrast 1;"..settings.default_contrast_value) end)
-mp.add_key_binding("1", "default_contrast_down",  function() mp.command("add contrast -1;"..settings.default_contrast_value) end)
+mp.add_key_binding("2", "default_contrast_up",  function() mp.command("add contrast 1;"..settings.default_contrast_value) end, "repeatable")
+mp.add_key_binding("1", "default_contrast_down",  function() mp.command("add contrast -1;"..settings.default_contrast_value) end, "repeatable")
 
 --亮度
-mp.add_key_binding("4", "default_brightness_up",  function() mp.command("add brightness 1;"..settings.default_brightness_value) end)
-mp.add_key_binding("3", "default_brightness_down",  function() mp.command("add brightness -1;"..settings.default_brightness_value) end)
+mp.add_key_binding("4", "default_brightness_up",  function() mp.command("add brightness 1;"..settings.default_brightness_value) end, "repeatable")
+mp.add_key_binding("3", "default_brightness_down",  function() mp.command("add brightness -1;"..settings.default_brightness_value) end, "repeatable")
 
 --伽马
-mp.add_key_binding("6", "default_gamma_up",  function() mp.command("add gamma 1;"..settings.default_gamma_value) end)
-mp.add_key_binding("5", "default_gamma_down",  function() mp.command("add gamma -1;"..settings.default_gamma_value) end)
+mp.add_key_binding("6", "default_gamma_up",  function() mp.command("add gamma 1;"..settings.default_gamma_value) end, "repeatable")
+mp.add_key_binding("5", "default_gamma_down",  function() mp.command("add gamma -1;"..settings.default_gamma_value) end, "repeatable")
 
 --饱和度
-mp.add_key_binding("8", "default_saturation_up",  function() mp.command("add saturation 1;"..settings.default_saturation_value) end)
-mp.add_key_binding("7", "default_saturation_down",  function() mp.command("add saturation -1;"..settings.default_saturation_value) end)
+mp.add_key_binding("8", "default_saturation_up",  function() mp.command("add saturation 1;"..settings.default_saturation_value) end, "repeatable")
+mp.add_key_binding("7", "default_saturation_down",  function() mp.command("add saturation -1;"..settings.default_saturation_value) end, "repeatable")
 
 
 
